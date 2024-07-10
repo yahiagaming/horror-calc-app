d89c7996216675f198f0a9935559f7dd95f57d55
yahiagaming <154459888+yahiagaming@users.noreply.github.com>
2024-07-11 00:46:32 +0300

    Add files via upload

---
+++ calc1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "warning the app contains horror things" << endl;
+    int first, second, choice;
+    string choiceS;
+
+    printf("first number? \n");
+    cin >> first;
+
+    printf("second number? \n");
+    cin >> second;
+
+    printf("choose operation: 1) +  2) - 3) x\n");
+    cin >> choice;
+
+
+   if (choice == 1){
+    cout << first << " + " << second << " = " << first + second;
+   }
+   else if(choice == 2){
+    cout << first << " - " << second << " = " << first - second;
+   }
+    else if (choice == 3){
+        cout << first << " x " << second << " = " << first * second;
+    }
+    else if (choice == 100){
+    cout << "how you get this secret" << endl;
+    cout << "made by two pepoles" << endl;
+    cout << "we make evrything from easy to hard" << endl;
+    cout << "thanks for downloading" << endl;
+    cout << "there is more codes but not a secret it is a helpful thing" << endl;
+
+
+}
+if (choice == 264){
+cout << "call evilhaker38@gmail.com in gmail or send a message to 01150204131" << endl;
+cout << "thanks for installing" << endl;
+cout << "thank you for trusting in me by two pepoles" << endl;
+}
+
+
+else if (choice == 112){
+cout << "welcome to the random number game" << endl;
+cout << "choose a number from 1 to 10"  << endl;
+cout << "under devloment" << endl;
+}
+else if (choice == 50){
+cout << "welcome to the new updates menu" << endl;
+cout << "the soon updates is a menu , more codes , more secrets" << endl;
+cout << "message email evilhaker38@gmail.com in gmail for more" << endl;
+}
+else if (choice == 780){
+cout << "want to make a new version for this app" << endl;
+cout << "call evilhaker38@gmail.com in gmail for the accept" << endl;
+
+
+}
+else if (choice == 666){
+cout << "don't look behind you" << endl;
+cout << 233+233;
+}
+else if (choice == 466){
+cout << "you will be kiled a few days" << endl;
+cout << "give 100000000 bitcoin or you well be kiled haahahahahha" << endl;
+}
+return 0;
+}
